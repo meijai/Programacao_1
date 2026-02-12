872b1b07b97c8a0480724bd21f09fc739248265d
meijai <meijai.exe@gmail.com>
2026-02-12 20:09:46 -0300

    aula 1

---
+++ Atividades/Nova pasta/Exemplo 01/Exemplo 01/Models/Animal.cs
@@ -0,0 +1,11 @@
+﻿namespace Exemplo_01.Models
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = null!;
+        public string Especie { get; set; } = null!;
+        public string Raca { get; set; } = null!;
+        public DateTime DataNascimento { get; set; }
+    }
+}
